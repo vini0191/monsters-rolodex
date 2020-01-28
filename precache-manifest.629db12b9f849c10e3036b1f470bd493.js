4e2642899e0d4ce2e4168fa1333c668c4aec9be4
Vinicius Alvim <vini0191@gmail.com>
2020-01-27 22:15:27 -0300

    Updates

---
+++ precache-manifest.629db12b9f849c10e3036b1f470bd493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e4244d6f648ba624dc7dda0e2342d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4efca5c1530205de543b",
+    "url": "/monsters-rolodex/static/css/main.1a54943d.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4efca5c1530205de543b",
+    "url": "/monsters-rolodex/static/js/main.9a9be714.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
